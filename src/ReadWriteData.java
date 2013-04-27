870b44c2e33b13e2c80916ef7a01fb15cc9917f9
Deniz Kocabas <kocabasd@hotmail.com>
2013-04-27 18:26:09 +0200

    Exercise 2

--- src/ReadWriteData.java
+++ src/ReadWriteData.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.text.DecimalFormat;
 import java.util.*;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
@@ -12,10 +11,10 @@ import org.apache.lucene.util.Version;
 
 
 public class ReadWriteData {
-	final static String TOPIC = "topic1";	
-	final static String CLASS = "misc.forsale";
-	final static String DOCUMENT_ID = "76057";
-	final static String SIZE = "large";
+	final static String TOPIC = "topic2";	
+	final static String CLASS = "talk.religion.misc";
+	final static String DOCUMENT_ID = "83561";
+	final static String SIZE = "bm25";
     // if the constraints FrequencyUpperThreshold, FrequencyLowerThreshold are 0, 
     // the program will not use the FrequencyThreshold
 	// large	FrequencyUpperThreshold = 0.0 FrequencyLowerThreshold = 0.0
@@ -23,6 +22,9 @@ public class ReadWriteData {
 	// small 	FrequencyUpperThreshold = 40.0	FrequencyLowerThreshold = 5.0
 	public static double FrequencyUpperThreshold = 0.0 ;
 	public static double FrequencyLowerThreshold = 0.0 ;	
+	// 0.5 < b < 0.8 and 1.2 < k1 < 2
+	public static double k_1 = 1.5 ;
+	public static double b = 0.75 ;
 	final static File FOLDER_NAME = new File("C:/20_newsgroups_subset/"+CLASS);
 	final static String OUTPUT_ARRF_FILE_NAME = "C:\\20_newsgroups_subset\\TextIndexer"+" "+SIZE;	
 	final static String OUTPUT_TXT_FILE_NAME = "C:\\20_newsgroups_subset\\"+SIZE+"_"+TOPIC+"_groupQ";	
@@ -31,6 +33,10 @@ public class ReadWriteData {
     static ArrayList<String> DOCLIST= new ArrayList<String>();
     static ArrayList<String> TERMLIST= new ArrayList<String>();
     static ArrayList<Integer> QUERY= new ArrayList<Integer>();
+    public static double [] Scores;
+    public static double [] Length;
+    public static double DocAvarageLength;
+    public static int Qid=0;
     static String [] TopK =new String[10];
     static double [] TopKScore =new double[10];
 	static ArrayList<Double> IDF= new ArrayList<Double>();
@@ -49,14 +55,42 @@ public class ReadWriteData {
 			data.readFile(i);
 		}
 		//System.out.println(list);
-		data.calculateTF();
+		data.docLength(queryID);
+		//data.calculateTF();	
+		/*for (int i =1; i<DOCLIST.size(); i++){
+			if (list.get(i).get(11) !="0"){
+				System.out.print(TERMLIST.get(i-1));
+				System.out.print(list.get(i).get(11)+";");
+			}			
+		}
+		system.out.println();*/
+		data.calculateNormalizeTF();
+		/*for (int i =1; i<DOCLIST.size(); i++){
+			if (list.get(i).get(11) !="0"){
+				System.out.print(TERMLIST.get(i-1));
+				System.out.print(list.get(i).get(11)+";");
+			}
+		}
+		System.out.println(); */
 		data.frequencyThresholding();		
 		data.multiplyIDF();
-		//System.out.println(IDF);
+		/*double count=0;
+		System.out.println(DOCLIST.get(11));
+		System.out.println(Length[11]);
+		System.out.println(DocAvarageLength);
+		for (int i =1; i<DOCLIST.size(); i++){
+			if (list.get(i).get(11) !="0"){
+				System.out.print(TERMLIST.get(i-1));
+				System.out.print(list.get(i).get(11)+";");
+			}
+			count +=Double.parseDouble(list.get(i).get(11));
+		}
+		System.out.println(count);*/
 		data.openFileToWrite(OUTPUT_ARRF_FILE_NAME+".arff");
 		//to write ArrfFile(), you need to comment the 2 function below => data.openFileToWrite(OUTPUT_TXT_FILE_NAME+".txt");		data.writeFile();
 		//data.writeArrfFile();
-		data.fastCosineScore(queryID);
+		data.calculateBM25Score();
+		//data.fastCosineScore();
 		data.openFileToWrite(OUTPUT_TXT_FILE_NAME+".txt");
 		data.writeFile();
 		data.closeFile();	
@@ -167,13 +201,15 @@ public class ReadWriteData {
 							  break;
 						  }
 					  }
-					  if(NewTerm) {					
+					  if(NewTerm) {
+						  if(temp[i].length()>3){
 						  TERMLIST.add(temp[i]);
 						  list.add(new ArrayList<String>());
 						  list.get(list.size()-1).add(temp[i]);
 				  	  	  for (k=0; k < DOCLIST.size();k++)
 				  	  		list.get(list.size()-1).add("0");
-				  	  	  list.get(list.size()-1).set(docId,"1.0");						  
+				  	  	  list.get(list.size()-1).set(docId,"1.0");	
+						  }
 					  }					  
 				  }	 
 			  }
@@ -203,6 +239,29 @@ public class ReadWriteData {
 	    }
 	    return sb.toString();	    
 	}
+	
+
+	public void docLength (String queryID){
+	    Scores =new double[DOCLIST.size()];
+	    Length =new double[DOCLIST.size()];
+		double count = 0;
+		
+		for (int i=1; i< DOCLIST.size(); i++){
+			Scores[i] =0 ;
+			for (int j=0; j< TERMLIST.size(); j++){
+				count += Double.parseDouble(list.get(j+1).get(i));
+				if (DOCLIST.get(i).equals(queryID)){
+					QUERY.add(j+1);
+					Qid = i;
+				}	
+				
+			}
+			Length[i]=count;
+			DocAvarageLength +=count;
+			count =0;
+		}
+		DocAvarageLength = DocAvarageLength /Length.length;
+	}
 
 	// calculate the TF score and change it in matrix
 	public void calculateTF(){
@@ -215,6 +274,24 @@ public class ReadWriteData {
 				}
 			}
 		}		
+	}
+
+	// calculate the Normalize TF score and change it in matrix
+	public void calculateNormalizeTF() {
+		double B, tf;		
+		// tf = tf / (tf + B * k_1);	
+		for (int i=1; i< list.get(0).size();i++){
+			B = b * Length[i]/DocAvarageLength;
+			B += 1-b;
+			for (int j=1; j< list.size();j++){
+				if (list.get(j).get(i)!= "0"){
+					tf = Double.parseDouble(list.get(j).get(i));
+					tf = tf / (tf + k_1*B);	
+					list.get(j).set(i,Double.toString(tf));
+				}
+			}
+		}
+		
 	}
 	
 	// to provide different nr of terms, using different frequency threshold
@@ -307,27 +384,32 @@ public class ReadWriteData {
 		}
 	}
 
-	public void fastCosineScore(String queryID){
-		double [] Scores =new double[DOCLIST.size()];
-		double [] Length =new double[DOCLIST.size()];
-		double count = 0;
-		int Qid=0;
-
-		for (int i=1; i< DOCLIST.size(); i++){
-			Scores[i] =0 ;
-			for (int j=0; j< TERMLIST.size(); j++){
-				if (list.get(j+1).get(i) != "0"){
-					count++;
-					if (DOCLIST.get(i).equals(queryID)){
-						QUERY.add(j+1);
-						Qid = i;
-					}	
+	public void calculateBM25Score() {
+		for(int j=1; j<DOCLIST.size(); j++){
+			for (int i=0; i < QUERY.size(); i++){				
+					if (j!= Qid)
+						Scores[j] += Double.parseDouble(list.get(QUERY.get(i)).get(j));
 				}
 			}
-			Length[i]=count;
-			count =0;
-		}
-
+			
+			double max=0;
+			int elementnr=0;
+			for (int i=0; i<10 ; i++){
+				for (int j=0; j<DOCLIST.size() ; j++){
+					if (max<Scores[j]){
+						max = Scores[j];
+						elementnr = j;
+					}
+				}
+				TopK[i]=DOCLIST.get(elementnr);
+				TopKScore [i]= Scores [elementnr];
+				Scores [elementnr]=0;
+				max=0;
+			}
+			
+	}
+	
+	public void fastCosineScore(){
 		for (int i=0; i < QUERY.size(); i++){
 			for(int j=1; j<DOCLIST.size(); j++){
 				if (j!= Qid)
@@ -352,10 +434,6 @@ public class ReadWriteData {
 			Scores [elementnr]=0;
 			max=0;
 		}		
-			
-		//for(int j=1; j<DOCLIST.size(); j++){
-		//	System.out.println(Scores[j]);	
-		//}
 	}
 	
 	public void writeFile(){	
